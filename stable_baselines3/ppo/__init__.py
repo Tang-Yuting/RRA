58b87737889cc054969c34ee6facdd656bf0db0a
tyt <tangyuting777@gmail.com>
2025-03-10 01:36:41 +0900

    Sync

--- stable_baselines3/ppo/__init__.py
+++
@@ -1,6 +0,0 @@
-from stable_baselines3.ppo.policies import CnnPolicy, MlpPolicy, MultiInputPolicy
-from stable_baselines3.ppo.ppo import PPO
-from stable_baselines3.ppo.max_ppo import MAX_PPO
-
-
-__all__ = ["PPO", "MAX_PPO", "CnnPolicy", "MlpPolicy", "MultiInputPolicy"]
